8763fc8b945cce23ec58a31c82e23827b2d4664c
Akile Altin <altinakile10@gmail.com>
2022-12-15 14:44:47 +0300

    first class added

---
+++ src/day01_javaIntro/test.java
@@ -0,0 +1,8 @@
+package day01_javaIntro;
+
+public class test {
+    public static void main(String[] args) {
+        String str="Wooden Spoon";
+        System.out.println("wooden spoon");
+    }
+}
