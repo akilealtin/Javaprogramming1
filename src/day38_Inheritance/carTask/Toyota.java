50baf1a7ecd10eb26b84542a56cacc96d116e7b2
Akile Altin <altinakile10@gmail.com>
2022-12-15 15:38:00 +0300

    day:Inheritance continue-method overriding

--- src/day38_Inheritance/carTask/Toyota.java
+++ src/day38_Inheritance/carTask/Toyota.java
@@ -1,5 +1,15 @@
 package day38_Inheritance.carTask;
 
-public class Toyota {
-
+public class Toyota extends Car {
+    public Toyota(String brand, String model, int year, double price, String color, int miles) {
+        super("Toyota", model, year, price, color, miles);
+    }
+    public void reliable(){
+        System.out.println(brand+" "+model+"is reliable");
+    }
 }
+/*
+toyota:
+ extra methods:
+ reliable()
+ */
