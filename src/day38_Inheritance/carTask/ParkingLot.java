f1cf2bd6822356704bf4a28ba1435fbcb77d33d1
Akile Altin <altinakile10@gmail.com>
2022-12-15 16:16:08 +0300

    day:Inheritance continue-method overriding

---
+++ src/day38_Inheritance/carTask/ParkingLot.java
@@ -0,0 +1,15 @@
+package day38_Inheritance.carTask;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+
+        Toyota toyota =new Toyota("Cambry","Cambry",2020,20000,"Gray",123456);
+
+        Tesla tesla =new Tesla("model s","model s",2020,94990,"red",0);
+
+        BMW bmw=new BMW("X5","X5",2020,67350,"black",12000);
+
+
+
+    }
+}
